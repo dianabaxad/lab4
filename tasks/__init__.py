b8f561a67c42302bcb10fc27b244d908e7dfa443
PC <dainka.smirnova.05@bk.ru>
2025-12-04 03:18:09 +0300

    init

---
+++ tasks/__init__.py
@@ -0,0 +1,8 @@
+"""
+Пакет tasks для выполнения задач по генераторам
+"""
+from .task1 import generate_combinations
+from .task2 import generate_function_values
+from .task3 import get_sort
+
+__all__ = ['generate_combinations', 'generate_function_values', 'get_sort']
